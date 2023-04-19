6dd122614931160e2da86ef192251f63f72fbbb7
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-04-19 15:32:19 +0530

    carts table added

--- krushi_database.sql
+++ krushi_database.sql
@@ -11,6 +11,8 @@ CREATE TABLE products(product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,product
  
 CREATE TABLE orders(order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,order_date DATETIME NOT NULL ,shipped_date DATETIME NOT NULL,cust_id INT NOT NULL,CONSTRAINT fk FOREIGN KEY (cust_id) REFERENCES customers(cust_id) ON UPDATE CASCADE ON DELETE CASCADE, total DOUBLE ,status ENUM('approved','initiated','cancelled','delivered','inprogress') NOT NULL);
 
+CREATE TABLE carts(cart_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,cust_id INT NOT NULL,CONSTRAINT fk_1 FOREIGN KEY (cust_id) REFERENCES customers(cust_id) ON UPDATE CASCADE ON DELETE CASCADE, total DOUBLE ,status ENUM('approved','initiated','cancelled','delivered','inprogress') NOT NULL);
+
 INSERT INTO users(email,password,contact_number) VALUES('shrisha12@gmail.com','shrisha@123','9850540298');
 INSERT INTO users(email,password,contact_number) VALUES('shivam12@gmail.com','shivam@123','7450540298');
 INSERT INTO users(email,password,contact_number) VALUES('kranti12@gmail.com','kranti@123','8850540298');
@@ -61,7 +63,24 @@ INSERT INTO products(product_title,unit_price,stock_available,image,category_id)
 INSERT INTO orders(order_date,shipped_date,cust_id,total,status) VALUES ('2020-12-01 12:12:12','2020-12-02 10:12:12',1,500,'initiated');
 INSERT INTO orders(order_date,shipped_date,cust_id,total,status) VALUES ('2020-11-01 12:12:12','2020-11-02 10:02:12',1,800,'delivered');
 INSERT INTO orders(order_date,shipped_date,cust_id,total,status) VALUES ('2020-10-01 12:12:12','2020-10-02 10:22:12',1,700,'cancelled');
+INSERT INTO orders(order_date,shipped_date,cust_id,total,status) VALUES ('2021-12-01 12:10:12','2021-12-02 10:12:12',1,500,'initiated');
+INSERT INTO orders(order_date,shipped_date,cust_id,total,status) VALUES ('2022-11-01 12:11:00','2022-11-02 10:02:12',2,1800,'delivered');
+INSERT INTO orders(order_date,shipped_date,cust_id,total,status) VALUES ('2023-10-01 12:13:11','2023-10-02 10:22:12',2,7100,'cancelled');
+INSERT INTO orders(order_date,shipped_date,cust_id,total,status) VALUES ('2020-05-01 12:14:13','2020-05-02 10:12:12',3,5020,'initiated');
+INSERT INTO orders(order_date,shipped_date,cust_id,total,status) VALUES ('2022-01-01 12:40:22','2022-01-02 10:02:12',3,8007,'delivered');
+INSERT INTO orders(order_date,shipped_date,cust_id,total,status) VALUES ('2023-10-01 12:55:45','2023-10-02 10:22:12',4,7006,'cancelled');
 
+INSERT INTO carts(cust_id) VALUES (1);
+INSERT INTO carts(cust_id) VALUES (1);
+INSERT INTO carts(cust_id) VALUES (2);
+INSERT INTO carts(cust_id) VALUES (2);
+INSERT INTO carts(cust_id) VALUES (2);
+INSERT INTO carts(cust_id) VALUES (3);
+INSERT INTO carts(cust_id) VALUES (3);
+INSERT INTO carts(cust_id) VALUES (4);
+INSERT INTO carts(cust_id) VALUES (3);
+INSERT INTO carts(cust_id) VALUES (3);
+INSERT INTO carts(cust_id) VALUES (4);
 
 
 
