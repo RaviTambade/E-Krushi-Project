fa57ad81e888ea744aa7f90beeceb27ccbe34ad7
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-04-21 16:14:56 +0530

    trigger for customer

--- krushi_database.sql
+++ krushi_database.sql
@@ -1,9 +1,12 @@
 
-CREATE DATABASE E_Krushi;
+drop DATABASE E_Krushi;
 USE E_Krushi;
 
 CREATE TABLE users(user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,email varchar(255) unique,password varchar(255),contact_number varchar(255));
-                   
+
+CREATE TABLE roles(role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, role varchar(250));
+              
+
 CREATE TABLE customers(cust_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,first_name VARCHAR(255),last_name VARCHAR(25),contact_number VARCHAR(255),email VARCHAR(50),password varchar(250));
                       
 CREATE TABLE categories(category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,category_title varchar(255),description varchar(255),image varchar(255));
@@ -20,13 +23,8 @@ CREATE TABLE addresses(address_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,cust_i
 
 CREATE TABLE cart_items(cart_items_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,cart_id INT NOT NULL,CONSTRAINT fk_02 FOREIGN KEY (cart_id) REFERENCES carts(cart_id)ON UPDATE CASCADE ON DELETE CASCADE,product_id INT NOT NULL,CONSTRAINT fk_03 FOREIGN KEY (product_id) REFERENCES products(product_id) ,quantity INT NOT NULL); 
 
-CREATE TABLE accounts(account_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, account_number varchar(250) UNIQUE,ifsc_code varchar(250),register_date DATETIME, user_id INT NOT NULL, CONSTRAINT fk_user_id_1 FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE);
 
 CREATE TABLE payments(payment_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,payment_date DATETIME NOT NULL , payment_mode ENUM('cash on delivery','online payment'),user_id INT NOT NULL,CONSTRAINT fk_user_id_5 FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,order_id INT NOT NULL,CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders(order_id) ON UPDATE CASCADE ON DELETE CASCADE);
-
-CREATE TABLE roles(role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, role varchar(250));
-
-CREATE TABLE user_roles(user_id INT NOT NULL,CONSTRAINT fk_user_id_2 FOREIGN KEY(user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE ,role_id INT NOT NULL,CONSTRAINT fk_role_id FOREIGN KEY(role_id) REFERENCES roles(role_id) ON UPDATE CASCADE ON DELETE CASCADE);
 
 CREATE TABLE departments(dept_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, dept_name VARCHAR(50),location VARCHAR(45));
 
@@ -36,9 +34,31 @@ CREATE TABLE shippers(shipper_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,company
 
 CREATE TABLE suppliers(supplier_id INT NOT NULL AUTO_iNCREMENT PRIMARY KEY, company_name varchar(50),supplier_name varchar(50),address varchar(50),contact_number VARCHAR(50),email varchar(50),password VARCHAR(50),city VARCHAR(50),state VARCHAR(40));
 
+
+
+CREATE TABLE accounts(account_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, account_number varchar(250) UNIQUE,ifsc_code varchar(250),register_date DATETIME, user_id INT NOT NULL, CONSTRAINT fk_user_id_1 FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE);
+
 CREATE TABLE transactions(transaction_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, from_account_number VARCHAR(60)NOT NULL,CONSTRAINT fk_acc_no FOREIGN KEY(from_account_number) REFERENCES accounts(account_number)ON UPDATE CASCADE ON DELETE CASCADE, to_account_number VARCHAR(60) NOT NULL,CONSTRAINT fk_acc_no1 FOREIGN KEY(to_account_number) REFERENCES accounts(account_number)ON UPDATE CASCADE ON DELETE CASCADE,transaction_date DATETIME NOT NULL, amount DOUBLE);
 
+CREATE TABLE user_roles(user_id INT NOT NULL,CONSTRAINT fk_user_id_2 FOREIGN KEY(user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE ,role_id INT NOT NULL,CONSTRAINT fk_role_id FOREIGN KEY(role_id) REFERENCES roles(role_id) ON UPDATE CASCADE ON DELETE CASCADE);
+   
+   
 CREATE TABLE feedbacks(feedback_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,description VARCHAR(255),user_id INT NOT NULL ,CONSTRAINT fk_022 FOREIGN KEY (user_id) REFERENCES users(user_id));
+
+
+-- TRIGGER FOR CUSTOMERS
+DELIMITER //
+CREATE TRIGGER insert_customer AFTER INSERT ON customers
+FOR EACH ROW
+BEGIN
+    DECLARE userId INT;
+    INSERT INTO users(email,password,contact_number) VALUES (NEW.email,NEW.password,NEW.contact_number);
+    SELECT user_id INTO userId FROM users WHERE email=NEW.email;
+    INSERT INTO user_roles(user_id,role_id) VALUES (userId,2); 
+END //
+
+DELIMITER ;
+
 
  -- USER TABLE DATA
 INSERT INTO users(email,password,contact_number) VALUES('shrisha12@gmail.com','shrisha@123','9850540298');
@@ -47,10 +67,21 @@ INSERT INTO users(email,password,contact_number) VALUES('kranti12@gmail.com','kr
 INSERT INTO users(email,password,contact_number) VALUES('rutujaaa12@gmail.com','pooja@123','9960540290');
 INSERT INTO users(email,password,contact_number) VALUES('pratikbhor12@gmail.com','pratik@123','9960540291');
 
+
+INSERT INTO roles(role) VALUES ('Admin');
+INSERT INTO roles(role) VALUES ('Customer');
+INSERT INTO roles(role) VALUES ('Employee');
+INSERT INTO roles(role) VALUES ('Supplier');
+INSERT INTO roles(role) VALUES ('Shipper');
+
+
+
+
 INSERT INTO customers(first_name,last_name,contact_number,email,password) VALUES('pratiksha','bangar','7834256798','pratikshabangar11@gmail.com','pratiksha@123');                       
 INSERT INTO customers(first_name,last_name,contact_number,email,password) VALUES('priyanka','jadhav','7834256783','priyankajadhav09@gmail.com','priyanka@123');
-INSERT INTO customers(first_name,last_name,contact_number,email,password) VALUES('pooja','divekar','9098556798','poojadivekar05@gmail.com','pooja@123');             
-INSERT INTO customers(first_name,last_name,contact_number,email,password) VALUES('pratik','bhor','9804256798','pratikbhor06@gmail.com','pratik@123');     
+INSERT INTO customers(first_name,last_name,contact_number,email,password) VALUES('pooja','divekar','9098556745','poojadivekar05@gmail.com','pooja@123');             
+INSERT INTO customers(first_name,last_name,contact_number,email,password) VALUES('pratik','bhor','9804256744','pratikbhor06@gmail.com','pratik@123');     
+INSERT INTO customers(first_name,last_name,contact_number,email,password) VALUES('pratik','VAVHAL','9804256796','pratik12@gmail.com','prati@123');     
 
 
 
@@ -127,13 +158,6 @@ INSERT INTO cart_items(cart_id,product_id ,quantity) VALUES(3,2,260);
 INSERT INTO cart_items(cart_id,product_id ,quantity) VALUES(4,3,200);
 INSERT INTO cart_items(cart_id,product_id ,quantity) VALUES(4,3,201);
 
-INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031201','KOTAK000286','2022-04-05  01:02:03',1);
-INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031202','ICICI000286','2022-11-25  01:12:03',1);
-INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031203','MAHB0000286','2022-12-30  11:02:03',3);
-INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031204','PDCC0000286','2022-05-10  12:02:03',2);
-INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031205','SBIB0000286','2022-09-07  06:02:03',2);
-INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031206','AXIS0000286','2022-11-03  07:02:03',4);
-INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031207','BARB0000286','2022-10-23  08:02:03',3);
 
 INSERT INTO payments(payment_date,payment_mode,user_id,order_id) VALUES('2022-03-08 12:08:19','cash on delivery',1,1);
 INSERT INTO payments(payment_date,payment_mode,user_id,order_id) VALUES('2022-03-08 12:08:19','online payment',2,1);
@@ -147,18 +171,6 @@ INSERT INTO addresses(cust_id,address_mode,house_number,landmark,city,state,coun
 INSERT INTO addresses(cust_id,address_mode,house_number,landmark,city,state,country,pincode) VALUES(3,'billing','houseNo.32','Peth-Kurwandi Road','Manchar','Maharashtra','India','410506');
 INSERT INTO addresses(cust_id,address_mode,house_number,landmark,city,state,country,pincode) VALUES(4,'permanent','houseNo.234','Pune-Nashik Highway','Rajgurunagar','Maharashtra','India','1213');
 
-INSERT INTO roles(role) VALUES ('Admin');
-INSERT INTO roles(role) VALUES ('Customer');
-INSERT INTO roles(role) VALUES ('Employee');
-INSERT INTO roles(role) VALUES ('Supplier');
-INSERT INTO roles(role) VALUES ('Shipper');
-
-INSERT INTO user_roles(user_id,role_id) VALUES (1,1);
-INSERT INTO user_roles(user_id,role_id) VALUES(2,1);
-INSERT INTO user_roles(user_id,role_id) VALUES(3,1);
-INSERT INTO user_roles(user_id,role_id) VALUES(6,2);
-INSERT INTO user_roles(user_id,role_id) VALUES(8,2);
-INSERT INTO user_roles(user_id,role_id) VALUES(6,3);
 
 
 INSERT INTO departments(dept_name,location) VALUES('account', 'pune');
@@ -183,6 +195,22 @@ INSERT INTO suppliers(company_name,supplier_name,address,contact_number,email,pa
 INSERT INTO suppliers(company_name,supplier_name,address,contact_number,email,password,city,state) VALUES('greenary','datta dhoble','manchar','8903456782','kaveri12@gmail.com','kaveri@123','pune','maharashtra');
 INSERT INTO suppliers(company_name,supplier_name,address,contact_number,email,password,city,state) VALUES('kavya','kavya bangar','chandoli','8903456782','kaveri12@gmail.com','kaveri@123','pune','maharashtra');
 
+
+INSERT INTO user_roles(user_id,role_id) VALUES (1,1);
+INSERT INTO user_roles(user_id,role_id) VALUES(2,1);
+INSERT INTO user_roles(user_id,role_id) VALUES(3,1);
+INSERT INTO user_roles(user_id,role_id) VALUES(6,2);
+INSERT INTO user_roles(user_id,role_id) VALUES(8,2);
+INSERT INTO user_roles(user_id,role_id) VALUES(6,3);
+
+INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031201','KOTAK000286','2022-04-05  01:02:03',1);
+INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031202','ICICI000286','2022-11-25  01:12:03',1);
+INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031203','MAHB0000286','2022-12-30  11:02:03',3);
+INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031204','PDCC0000286','2022-05-10  12:02:03',2);
+INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031205','SBIB0000286','2022-09-07  06:02:03',2);
+INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031206','AXIS0000286','2022-11-03  07:02:03',4);
+INSERT INTO accounts(account_number,ifsc_code,register_date,user_id) VALUES('4105031207','BARB0000286','2022-10-23  08:02:03',3);
+
 INSERT INTO transactions(from_account_number,to_account_number,transaction_date,amount) VALUES('4105031201','4105031202','2022-03-09  08:45:23',45000);
 INSERT INTO transactions(from_account_number,to_account_number,transaction_date,amount) VALUES('4105031203','4105031205','2022-12-09  01:40:10',12000);
 INSERT INTO transactions(from_account_number,to_account_number,transaction_date,amount) VALUES('4105031204','4105031206','2022-08-09  10:30:20',25000);
@@ -205,13 +233,16 @@ SELECT * FROM customers;
 SELECT * FROM customers WHERE cust_id=1;  
 
 -- UPDATE CUSTOMER BY ID;
-UPDATE customers SET contact_number='9881751213',password ='aj123' WHERE cust_id=1;
+-- UPDATE customers SET contact_number='9881751213',password ='aj123' WHERE cust_id=1;
 
 -- DELETE CUSTOMER BY ID
-DELETE FROM customers WHERE cust_id=4;
+-- DELETE FROM customers WHERE cust_id=4;
 
-select * from user_roles;
+select * from roles;
 select * from users;
+select * from customers;
+desc user_roles;
+
 
 -- when we give the name of customer then we give role of this customer
 select role from roles where role_id IN (select role_id from user_roles where user_id IN (select user_id from users where password IN (select password from customers where first_name='pooja' and last_name='divekar')));
