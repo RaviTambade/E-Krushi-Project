1765c961a3baa5483827b10509623407dc48ccb8
PragatiBangar <bangarpragati11@gmail.com>
2023-05-25 20:32:12 +0530

    users service deleted

--- users/Model/User.cs
+++
@@ -1,9 +0,0 @@
-namespace E_krushiApp.Models;
-public class User{
-
-    public  int UserId{get;set;}
-    public string? Email {get;set;}
-    public string? Password {get;set;}
-
-    public string? ContactNumber {get;set;}
-}
