1187e0ceed3440e915f634b221f3376f85460f71
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-05-26 21:36:57 +0530

    new changes

---
+++ Consulting/Models/QesAns.cs
@@ -0,0 +1,15 @@
+
+
+namespace  E_krushiApp.Models{
+
+
+    public class QesAns{
+
+
+        public int  id{get;set;}
+
+        public string question{get;set;}
+
+        public string solution{get;set;}
+    }
+}
