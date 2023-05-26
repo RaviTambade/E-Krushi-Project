2aeb7f62c913d09fa9e4a99c3aaf0d414a5191d4
Ravi Tambade <ravi.tambade@transflower.in>
2023-05-26 11:30:02 +0530

    Changes suggested for consulting

--- AgreeDoctorsService/Models/Question.cs
+++ AgreeDoctorsService/Models/Question.cs
@@ -3,10 +3,7 @@ public class Question{
 
     public int Id{get;set;}
     public DateTime Date{get;set;}
-
     public string Description{get;set;}
-
     public int CustId{get;set;}
-
     public int CategoryId{get;set;}
 }
--- AgreeDoctorsService/Models/QuestionSolution.cs
+++ AgreeDoctorsService/Models/QuestionSolution.cs
@@ -2,13 +2,8 @@ namespace E_krushiApp.Models;
 public class QuestionSolution{
 
     public int QuestionId{get;set;}
-    
     public int SolutionId{get;set;}
-    
     public DateTime SolutionDate{get;set;}
-
-
     public int AgriDoctorId{get;set;}
 
-   
 }
