5c35da5772313230aafb9075244e7d405446e3d5
PragatiBangar <bangarpragati11@gmail.com>
2023-07-04 20:05:03 +0530

    stored procedure added for create order

--- Database/spdb.sql
+++ Database/spdb.sql
@@ -10,3 +10,30 @@ COMMIT;
 END $$
 DELIMITER ;
 
+
+DELIMITER $$
+CREATE PROCEDURE CreateOrder(IN cartId BIGINT )
+BEGIN
+DECLARE noMoreRow INT default 0; 
+DECLARE orderId INT;
+DECLARE productId INT;
+DECLARE quantity INT;
+DECLARE custId INT;
+DECLARE cart_cursor CURSOR  FOR SELECT cart_items.product_id,cart_items.quantity FROM cart_items WHERE cart_id=cartId; 
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET noMoreRow = 1;
+SELECT cust_id INTO custId FROM carts WHERE cart_id=cartId;
+INSERT INTO orders (order_date, cust_id,total,status) VALUES (now(),custId,0,'initiated');
+SET orderId=last_insert_id();
+OPEN cart_cursor ;
+cart_items:LOOP
+    FETCH cart_cursor INTO productId,quantity;
+    IF noMoreRow=1 THEN 
+		LEAVE cart_items;
+    END IF;
+     INSERT INTO orderdetails(order_id, product_id, quantity)VALUES(orderId,productId,quantity); 
+END LOOP cart_items;
+DELETE FROM cart_items WHERE cart_id=cartId;
+CLOSE cart_cursor;
+END $$
+DELIMITER ;
+-- CALL CreateOrder();
