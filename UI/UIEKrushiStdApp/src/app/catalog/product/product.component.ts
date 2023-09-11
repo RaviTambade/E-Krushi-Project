0731269eb34275ef3f2e0c2df81dd6a784bfab2c
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-09-11 21:07:00 +0530

    new changes in product home page

--- UI/UIEKrushiStdApp/src/app/catalog/product/product.component.ts
+++ UI/UIEKrushiStdApp/src/app/catalog/product/product.component.ts
@@ -9,42 +9,42 @@ export class ProductComponent {
   price :number=100;
 
   products:any =[{
-    imagepath:"/assets/mira.webp" , size :"250 gm" ,price : 100
+    imagepath:"/assets/mira.webp" , size :"250 gm" ,price : 100,name : "Admire"
   },
   {
-    imagepath:"/assets/Rogor.jpeg" , size :"250 gm" ,price : 480
+    imagepath:"/assets/Rogor.jpeg" , size :"250 gm" ,price : 480,name : "ROGOR"
   },{
-    imagepath:"/assets/UREA.png" , size :"250 gm" ,price : 45
+    imagepath:"/assets/UREA.png" , size :"250 gm" ,price : 45,name : "UREA"
   },
   
   {
-    imagepath:"/assets/antracol.jpeg" , size :"250 gm" ,price : 145
+    imagepath:"/assets/antracol.jpeg" , size :"250 gm" ,price : 145,name : "ANTARCOL"
   },  
   {
-    imagepath:"/assets/Avtar.jpeg" , size :"250 gm" ,price : 115
+    imagepath:"/assets/Avtar.jpeg" , size :"250 gm" ,price : 115, name : "AVTAR"
   },  
   {
-    imagepath:"/assets/coragen.jpeg" , size :"250 gm" ,price : 110
+    imagepath:"/assets/coragen.jpeg" , size :"250 gm" ,price : 110, name : "CORAGEN"
   },  
   {
-    imagepath:"/assets/zyme.jpeg" , size :"250 gm" ,price : 450
+    imagepath:"/assets/zyme.jpeg" , size :"250 gm" ,price : 450, name : "ZYME"
   }, 
   {
-    imagepath:"/assets/Melody.jpeg" , size :"250 gm" ,price : 550
+    imagepath:"/assets/Melody.jpeg" , size :"250 gm" ,price : 550 ,name : "MELODY"
   }, 
   {
-    imagepath:"/assets/10-26-26.jpeg" , size :"250 gm" ,price : 400
+    imagepath:"/assets/10-26-26.jpeg" , size :"250 gm" ,price : 400 ,name : "10-26-26"
   },  
   {
-    imagepath:"/assets/12-32-16.jpeg" , size :"250 gm" ,price : 600
+    imagepath:"/assets/12-32-16.jpeg" , size :"250 gm" ,price : 600,name : "12-32-16"
   },  
    {
-    imagepath:"/assets/Goal.jpeg" , size :"250 gm" ,price : 200
+    imagepath:"/assets/Goal.jpeg" , size :"250 gm" ,price : 200 ,name : "GOAL"
   },   
 
 
   {
-    imagepath:"/assets/targa-super.png" , size :"250 gm" ,price : 500
+    imagepath:"/assets/targa-super.png" , size :"250 gm" ,price : 500,name : "TARGA SUPER"
   }]
 
 }
