d99fbebf4fd14a9a5a1684e7496f5b28d0dc41d0
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-09-15 16:45:17 +0530

    search bar component works

--- UI/UIEKrushiStdApp/src/app/catalog/product/product.component.ts
+++ UI/UIEKrushiStdApp/src/app/catalog/product/product.component.ts
@@ -1,60 +1,104 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
-  price :number=100;
+export class ProductComponent implements OnInit {
+  price: number = 100;
+  searchString: string | null = null;
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.searchString = params.get('input');
+      if (this.searchString != null) {
+        this.products = this.filterProducts(this.searchString);
+      }
+    });
+  }
+  products: any[] = [
+    {
+      imageUrl: '/assets/mira.webp',
+      size: '250 gm',
+      price: 100,
+      title: 'Admire',
+    },
+    {
+      imageUrl: '/assets/Rogor.jpeg',
+      size: '250 gm',
+      price: 480,
+      title: 'ROGOR',
+    },
+    {
+      imageUrl: '/assets/UREA.png',
+      size: '250 gm',
+      price: 45,
+      title: 'UREA',
+    },
 
-  constructor(private router:Router){}
-  products:any =[{
-    imageUrl:"/assets/mira.webp" , size :"250 gm" ,price : 100,title : "Admire"
-  },
-  {
-    imageUrl:"/assets/Rogor.jpeg" , size :"250 gm" ,price : 480,title : "ROGOR"
-  },{
-    imageUrl:"/assets/UREA.png" , size :"250 gm" ,price : 45,title : "UREA"
-  },
-  
-  {
-    imageUrl:"/assets/antracol.jpeg" , size :"250 gm" ,price : 145,title : "ANTARCOL"
-  },  
-  {
-    imageUrl:"/assets/Avtar.jpeg" , size :"250 gm" ,price : 115, title : "AVTAR"
-  },  
-  {
-    imageUrl:"/assets/coragen.jpeg" , size :"250 gm" ,price : 110, title : "CORAGEN"
-  },  
-  {
-    imageUrl:"/assets/zyme.jpeg" , size :"250 gm" ,price : 450, title : "ZYME"
-  }, 
-  {
-    imageUrl:"/assets/Melody.jpeg" , size :"250 gm" ,price : 550 ,title : "MELODY"
-  }, 
-  {
-    imageUrl:"/assets/10-26-26.jpeg" , size :"250 gm" ,price : 400 ,title : "10-26-26"
-  },  
-  {
-    imageUrl:"/assets/12-32-16.jpeg" , size :"250 gm" ,price : 600,title : "12-32-16"
-  },  
-   {
-    imageUrl:"/assets/Goal.jpeg" , size :"250 gm" ,price : 200 ,title : "GOAL"
-  },   
+    {
+      imageUrl: '/assets/antracol.jpeg',
+      size: '250 gm',
+      price: 145,
+      title: 'ANTARCOL',
+    },
+    {
+      imageUrl: '/assets/Avtar.jpeg',
+      size: '250 gm',
+      price: 115,
+      title: 'AVTAR',
+    },
+    {
+      imageUrl: '/assets/coragen.jpeg',
+      size: '250 gm',
+      price: 110,
+      title: 'CORAGEN',
+    },
+    {
+      imageUrl: '/assets/zyme.jpeg',
+      size: '250 gm',
+      price: 450,
+      title: 'ZYME',
+    },
+    {
+      imageUrl: '/assets/Melody.jpeg',
+      size: '250 gm',
+      price: 550,
+      title: 'MELODY',
+    },
+    {
+      imageUrl: '/assets/10-26-26.jpeg',
+      size: '250 gm',
+      price: 400,
+      title: '10-26-26',
+    },
+    {
+      imageUrl: '/assets/12-32-16.jpeg',
+      size: '250 gm',
+      price: 600,
+      title: '12-32-16',
+    },
+    {
+      imageUrl: '/assets/Goal.jpeg',
+      size: '250 gm',
+      price: 200,
+      title: 'GOAL',
+    },
 
+    {
+      imageUrl: '/assets/targa-super.png',
+      size: '250 gm',
+      price: 500,
+      title: 'TARGA SUPER',
+    },
+  ];
 
-  {
-    imageUrl:"/assets/targa-super.png" , size :"250 gm" ,price : 500,title : "TARGA SUPER"
-  }]
-
-
-
-
-
-  
-
+  filterProducts(searchString: string) {
+    var products = this.products.filter((p) => p.title == searchString);
+    return products;
+  }
 }
--- UI/UIEKrushiStdApp/src/app/catalog/searchbar/searchbar.component.ts
+++ UI/UIEKrushiStdApp/src/app/catalog/searchbar/searchbar.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from 'src/app/Services/order-service.service';
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
   styleUrls: ['./searchbar.component.css']
 })
-export class SearchbarComponent {
+export class SearchbarComponent  {
+   
+
+  searchString:string="";
+  constructor(private router:Router){}
+
+   onSearch(){
+    console.log(this.searchString)
+   this.router.navigate(['catalog/products',this.searchString]) ;
+   }
+   
+  // products:any[]=[{
+  //   imageUrl:"/assets/mira.webp" , size :"250 gm" ,price : 100,title : "Admire"
+  // },
+  // {
+  //   imageUrl:"/assets/Rogor.jpeg" , size :"250 gm" ,price : 480,title : "ROGOR"
+  // },{
+  //   imageUrl:"/assets/UREA.png" , size :"250 gm" ,price : 45,title : "UREA"
+  // },
+  
+  // {
+  //   imageUrl:"/assets/antracol.jpeg" , size :"250 gm" ,price : 145,title : "ANTARCOL"
+  // },  
+  // {
+  //   imageUrl:"/assets/Avtar.jpeg" , size :"250 gm" ,price : 115, title : "AVTAR"
+  // },  
+  // {
+  //   imageUrl:"/assets/coragen.jpeg" , size :"250 gm" ,price : 110, title : "CORAGEN"
+  // },  
+  // {
+  //   imageUrl:"/assets/zyme.jpeg" , size :"250 gm" ,price : 450, title : "ZYME"
+  // }, 
+  // {
+  //   imageUrl:"/assets/Melody.jpeg" , size :"250 gm" ,price : 550 ,title : "MELODY"
+  // }, 
+  // {
+  //   imageUrl:"/assets/10-26-26.jpeg" , size :"250 gm" ,price : 400 ,title : "10-26-26"
+  // },  
+  // {
+  //   imageUrl:"/assets/12-32-16.jpeg" , size :"250 gm" ,price : 600,title : "12-32-16"
+  // },  
+  //  {
+  //   imageUrl:"/assets/Goal.jpeg" , size :"250 gm" ,price : 200 ,title : "GOAL"
+  // },   
+
+
+  // {
+  //   imageUrl:"/assets/targa-super.png" , size :"250 gm" ,price : 500,title : "TARGA SUPER"
+  // }]
+
+  
+  // onSelectedProduct(){
+  //   console.log(this.selectedProduct);
+  //   const sortedProducts=this.products.filter(u=>u.title===this.selectedProduct)
+  //   console.log(sortedProducts);
+  //   this.products=sortedProducts;
+
+  // }
+
+
 
 }
