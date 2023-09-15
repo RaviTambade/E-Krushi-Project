59b0c2b5b990ea80f8028f5d84d6858d40c1d8a9
Sahil <sahilmankar311@gmail.com>
2023-09-15 16:35:43 +0530

    product component updated

--- UI/UIEKrushiStdApp/src/app/catalog/product/product.component.ts
+++ UI/UIEKrushiStdApp/src/app/catalog/product/product.component.ts
@@ -1,60 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
-  price :number=100;
+export class ProductComponent implements OnInit {
 
 
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+  }
+  products: any = [
+    {
+      imageUrl: '/assets/mira.webp',
+      size: '250 gm',
+      price: 100,
+      title: 'Admire',
+      rating:3.5
+    },
+    {
+      imageUrl: '/assets/Rogor.jpeg',
+      size: '250 gm',
+      price: 480,
+      title: 'ROGOR',
+      rating:4.5
 
-  constructor(private router:Router){}
-  products:any =[{
-    imageUrl:"/assets/mira.webp" , size :"250 gm" ,price : 100,title : "Admire"
-  },
-  {
-    imageUrl:"/assets/Rogor.jpeg" , size :"250 gm" ,price : 480,title : "ROGOR"
-  },{
-    imageUrl:"/assets/UREA.png" , size :"250 gm" ,price : 45,title : "UREA"
-  },
-  
-  {
-    imageUrl:"/assets/antracol.jpeg" , size :"250 gm" ,price : 145,title : "ANTARCOL"
-  },  
-  {
-    imageUrl:"/assets/Avtar.jpeg" , size :"250 gm" ,price : 115, title : "AVTAR"
-  },  
-  {
-    imageUrl:"/assets/coragen.jpeg" , size :"250 gm" ,price : 110, title : "CORAGEN"
-  },  
-  {
-    imageUrl:"/assets/zyme.jpeg" , size :"250 gm" ,price : 450, title : "ZYME"
-  }, 
-  {
-    imageUrl:"/assets/Melody.jpeg" , size :"250 gm" ,price : 550 ,title : "MELODY"
-  }, 
-  {
-    imageUrl:"/assets/10-26-26.jpeg" , size :"250 gm" ,price : 400 ,title : "10-26-26"
-  },  
-  {
-    imageUrl:"/assets/12-32-16.jpeg" , size :"250 gm" ,price : 600,title : "12-32-16"
-  },  
-   {
-    imageUrl:"/assets/Goal.jpeg" , size :"250 gm" ,price : 200 ,title : "GOAL"
-  },   
+    },
+    {
+      imageUrl: '/assets/UREA.png',
+      size: '250 gm',
+      price: 45,
+      title: 'UREA',
+      rating:3
 
+    },
 
-  {
-    imageUrl:"/assets/targa-super.png" , size :"250 gm" ,price : 500,title : "TARGA SUPER"
-  }]
+    {
+      imageUrl: '/assets/antracol.jpeg',
+      size: '250 gm',
+      price: 145,
+      title: 'ANTARCOL',
+      rating:3.5
 
+    },
+    {
+      imageUrl: '/assets/Avtar.jpeg',
+      size: '250 gm',
+      price: 115,
+      title: 'AVTAR',
+      rating:2.5
 
+    },
+    {
+      imageUrl: '/assets/coragen.jpeg',
+      size: '250 gm',
+      price: 110,
+      title: 'CORAGEN',
+      rating:4
 
+    },
+    {
+      imageUrl: '/assets/zyme.jpeg',
+      size: '250 gm',
+      price: 450,
+      title: 'ZYME',
+      rating:3.5
 
+    },
+    {
+      imageUrl: '/assets/Melody.jpeg',
+      size: '250 gm',
+      price: 550,
+      title: 'MELODY',
+      rating:3.5
 
-  
+    },
+    {
+      imageUrl: '/assets/10-26-26.jpeg',
+      size: '250 gm',
+      price: 400,
+      title: '10-26-26',
+      rating:3.5
 
+    },
+    {
+      imageUrl: '/assets/12-32-16.jpeg',
+      size: '250 gm',
+      price: 600,
+      title: '12-32-16',
+      rating:3.5
+
+    },
+    {
+      imageUrl: '/assets/Goal.jpeg',
+      size: '250 gm',
+      price: 200,
+      title: 'GOAL',
+      rating:3.5
+
+    },
+
+    {
+      imageUrl: '/assets/targa-super.png',
+      size: '250 gm',
+      price: 500,
+      title: 'TARGA SUPER',
+      rating:3.5
+
+    },
+  ];
 }
