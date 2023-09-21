d75a4156b69828f8115b182c8eac86007174f35c
Sahil <sahilmankar311@gmail.com>
2023-09-21 17:15:53 +0530

    cart summary component added

--- UI/UIEKrushiStdApp/src/app/catalog/shoppingcart/shoppingcart.component.ts
+++ UI/UIEKrushiStdApp/src/app/catalog/shoppingcart/shoppingcart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CartItem } from 'src/app/Models/cart-item';
@@ -12,20 +12,38 @@ import { DeleteConfirmationComponent } from 'src/app/delete-confirmation/delete-
 })
 export class ShoppingcartComponent implements OnInit {
   items: CartItem[] = [];
+  totalItems: string = '';
+  subTotal: number = 0;
+  discount: number = 0;
+  shippingCharges = 'Free';
+  total: number = 0;
+
+  maxAllowedQuantity: number = 10;
+  minAllowedQuantity: number = 1;
   constructor(
     private cartsvc: CartService,
     private snackBar: MatSnackBar,
     private dialog: MatDialog
   ) {}
+
   ngOnInit(): void {
     this.cartsvc.getCartItems().subscribe((res) => {
       this.items = res;
+      this.calculateSummary();
     });
   }
 
-  openDeleteConfirmationDialog(cartItemId:number) {
-    const dialogRef = this.dialog.open(DeleteConfirmationComponent, {
-    });
+  private calculateSummary() {
+    this.totalItems = this.items.length.toString();
+    this.subTotal = 0;
+    this.items
+      .map((item) => item.quantity * item.unitPrice)
+      .forEach((i) => (this.subTotal += i));
+    this.total = this.subTotal - this.discount;
+  }
+
+  openDeleteConfirmationDialog(cartItemId: number) {
+    const dialogRef = this.dialog.open(DeleteConfirmationComponent, {});
 
     dialogRef.afterClosed().subscribe((result) => {
       if (result === true) {
@@ -39,6 +57,7 @@ export class ShoppingcartComponent implements OnInit {
     this.cartsvc.RemoveItem(cartItemId).subscribe((res) => {
       if (res) {
         this.items = this.items.filter((item) => item.cartItemId != cartItemId);
+        this.calculateSummary();
       }
     });
   }
@@ -53,40 +72,50 @@ export class ShoppingcartComponent implements OnInit {
 
   onClickDecrement(item: CartItem) {
     let newQuantity = item.quantity - 1;
-    if (newQuantity < 1) {
-      newQuantity = 1;
+    if (newQuantity < this.minAllowedQuantity) {
+      newQuantity = this.minAllowedQuantity;
     }
     this.updateDatabaseQuantity(item, newQuantity);
   }
   onClickIncrement(item: CartItem) {
     let newQuantity = item.quantity + 1;
 
-    if (newQuantity > 10) {
-      newQuantity = 10;
+    if (newQuantity > this.maxAllowedQuantity) {
+      newQuantity = this.maxAllowedQuantity;
     }
     this.updateDatabaseQuantity(item, newQuantity);
   }
 
   updateQuantity(item: CartItem, event: any) {
     let newQuantity = event.target.value;
-    if (newQuantity > 10) {
-      newQuantity = 10;
+    if (newQuantity > this.maxAllowedQuantity) {
+      newQuantity = this.maxAllowedQuantity;
     }
-    if (newQuantity < 1) {
-      newQuantity = 1;
+    if (newQuantity < this.minAllowedQuantity) {
+      newQuantity = this.minAllowedQuantity;
     }
 
     this.updateDatabaseQuantity(item, newQuantity);
   }
 
   updateDatabaseQuantity(item: CartItem, quantity: number) {
+    if ( quantity == item.quantity &&  quantity == this.maxAllowedQuantity) {
+      this.showNotification(`Maximum ${this.maxAllowedQuantity} Quantity Per Order is allowed`);
+      return;
+    }
+    if ( quantity == item.quantity &&  quantity == this.minAllowedQuantity) {
+      this.showNotification(`Minimum ${this.minAllowedQuantity} Quantity Per Order is allowed`);
+      return;
+    }
     this.cartsvc.updateQuantity(item.cartItemId, quantity).subscribe((res) => {
       if (res) {
         this.showNotification(
           `You  Changed  ${item.title}  Quantity  To  ${quantity}`
         );
-        item.quantity = quantity;
       }
+      item.quantity = quantity;
+      console.log(this.items);
+      this.calculateSummary();
     });
   }
 }
