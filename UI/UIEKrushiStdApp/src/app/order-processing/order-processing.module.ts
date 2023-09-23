a92441e8719a3ddb580a6c4a7110f205e8d68de5
Sahil <sahilmankar311@gmail.com>
2023-09-23 17:22:59 +0530

    address and payment component updated

--- UI/UIEKrushiStdApp/src/app/order-processing/order-processing.module.ts
+++ UI/UIEKrushiStdApp/src/app/order-processing/order-processing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddressComponent } from './address/address.component';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AddAddressComponent } from './add-address/add-address.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddressMainComponent } from './address-main/address-main.component';
+import { PaymentComponent } from './payment/payment.component';
 
 export const orderProcessingRoutes: Routes = [
-  { path: 'address/add', component: AddAddressComponent },
-  { path: 'address', component: AddressComponent },
+  {
+    path: 'address',
+    component: AddressMainComponent,
+  },
+  {
+    path:'payment',
+    component:PaymentComponent
+  }
 ];
 
 @NgModule({
-  declarations: [AddressComponent, AddAddressComponent],
-  imports: [CommonModule,ReactiveFormsModule,FormsModule],
+  declarations: [AddressComponent, AddAddressComponent, AddressMainComponent, PaymentComponent],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterModule],
 })
 export class OrderProcessingModule {}
