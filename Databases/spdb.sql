3633c3f231e198367057b0f9271f39c650fcc24f
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-10-06 14:40:41 +0530

    stored procedure for todays,yesterday,weekly,monthly orders

--- Databases/spdb.sql
+++ Databases/spdb.sql
@@ -1,4 +1,4 @@
--- Active: 1682349138553@@127.0.0.1@3306@ekrushi
+-- Active: 1694968636816@@127.0.0.1@3306@ekrushi
 
 
 CREATE PROCEDURE insertpayment(
@@ -40,7 +40,34 @@ BEGIN
 END;
 
 
+CREATE PROCEDURE OrdersByDate
+(
+    IN given_date DATE
+)
+BEGIN
+  --todays orders query
+    SELECT *
+    FROM Orders
+    WHERE DATE(orderdate) = given_date;
 
 
+--yesterdays order query
+    SELECT *
+    FROM Orders
+    WHERE DATE(orderdate) = DATE_SUB(given_date, INTERVAL 1 DAY);
+
+ --weekly orders
+    SELECT *
+    FROM Orders
+    WHERE DATE(orderdate) BETWEEN DATE_SUB(given_date, INTERVAL (DAYOFWEEK(given_date) - 1) DAY) 
+    AND DATE_ADD(given_date, INTERVAL (7 - DAYOFWEEK(given_date)) DAY);
+
+    --monthly orders
+     SELECT *
+    FROM Orders
+    WHERE DATE(orderdate) BETWEEN DATE_SUB('2023-09-06', INTERVAL DAY('2023-09-06') - 1 DAY)
+    AND LAST_DAY('2023-09-06');
+END;
 
 
+CALL OrdersByDate('2023-09-06');
