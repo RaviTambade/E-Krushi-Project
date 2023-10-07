1971ead31b3a91d09a255d5ff807a5beb325ab68
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-10-07 09:43:10 +0530

    stored procedure changes done

--- Databases/spdb.sql
+++ Databases/spdb.sql
@@ -1,4 +1,4 @@
--- Active: 1678359546568@@127.0.0.1@3306@ekrushi
+-- Active: 1694968636816@@127.0.0.1@3306@ekrushi
 
 
 CREATE PROCEDURE insertpayment(
@@ -43,6 +43,7 @@ END;
 CREATE PROCEDURE GetOrdersByDate
 (
     IN given_date DATE,
+    IN givenStoreId INT,
     OUT todaysOrders INT,
     OUT yesterdaysOrders INT,
     OUT weekOrders INT,
@@ -52,26 +53,31 @@ BEGIN
   
     SELECT count(*) INTO todaysOrders
     FROM Orders
-    WHERE DATE(orderdate) = given_date;
+    WHERE DATE(orderdate) = given_date  AND storeid=givenStoreId;
 
 
     SELECT count(*) INTO yesterdaysOrders
     FROM Orders
-    WHERE DATE(orderdate) = DATE_SUB(given_date, INTERVAL 1 DAY);
+    WHERE DATE(orderdate) = DATE_SUB(given_date, INTERVAL 1 DAY)AND storeid=givenStoreId;
 
  
     SELECT count(*) INTO weekOrders
     FROM Orders
     WHERE DATE(orderdate) BETWEEN DATE_SUB(given_date, INTERVAL (DAYOFWEEK(given_date) - 1) DAY) 
-    AND DATE_ADD(given_date, INTERVAL (7 - DAYOFWEEK(given_date)) DAY); 
+    AND DATE_ADD(given_date, INTERVAL (7 - DAYOFWEEK(given_date)) DAY) AND storeid=givenStoreId; 
   
      SELECT count(*) INTO monthOrders
     FROM Orders
     WHERE DATE(orderdate) BETWEEN DATE_SUB(given_date, INTERVAL DAY(given_date) - 1 DAY)
-    AND LAST_DAY(given_date);
+    AND LAST_DAY(given_date) AND storeid=givenStoreId;
 END;
 
 
-CALL GetOrdersByDate('2023-09-10',@todaysOrders,@yesterdaysOrders,@weekOrders,@monthOrders);
+CALL GetOrdersByDate('2023-07-26',1,@todaysOrders,@yesterdaysOrders,@weekOrders,@monthOrders);
 
 SELECT @todaysOrders,@yesterdaysOrders,@weekOrders,@monthOrders ;
+
+
+
+
+select * from orders where orderdate BETWEEN '2023-07-01' AND  '2023-07-31' and storeid=1;
